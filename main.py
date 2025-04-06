6ebc871a14dee903d6b99c7e69875b9ae78daa5b
EyaBouajila <ebouajila20001304@gmail.com>
2025-04-06 19:40:26 +0100

    update main with env variables

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+from routes import base
 
 app = FastAPI()
-
-
-@app.get("/")
-def welcome():
-    return {"message": "Hello Touta"}
+app.include_router(base.base_router)
